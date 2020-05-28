12b04284cad4a2bf720fe6a27748dd35b0c34172
Yosuke Nanao <yosuke.nanao@gmail.com>
2020-05-28 23:19:25 +0000

    modify login-password-form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :log_in
 
-  private
-    def current_user
-      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 現在ログイン中のユーザーを返す (いる場合)
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
     end
+  end
 end
